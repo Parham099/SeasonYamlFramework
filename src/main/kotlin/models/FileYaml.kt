5492c8195200dd3dfc58ac737e49ac09632001ec
Parham099 <parhamahmady522@gmail.com>
2026-07-28 01:13:16 +0330

    0.0.2-B fix: Some issue and runtime errors

--- src/main/kotlin/models/FileYaml.kt
+++ src/main/kotlin/models/FileYaml.kt
@@ -45,14 +45,19 @@ class FileYaml(val yamlFile: File) : SeasonYaml {
     }
 
     private fun getByPath(path: String): Any? {
-        val pathArray = path.split('.')
-        var data: MutableMap<String, Any?> = yamlData
-        for ((index, arg) in pathArray.withIndex()) {
-            if (index != pathArray.lastIndex) {
-                data = data[arg] as MutableMap<String, Any?>
-            } else {
-                return data[arg]
+        try {
+            val pathArray = path.split('.')
+            var data: MutableMap<String, Any?> = yamlData
+            for ((index, arg) in pathArray.withIndex()) {
+                if (index != pathArray.lastIndex) {
+                    data = data[arg] as MutableMap<String, Any?>
+                } else {
+                    return data[arg]
+                }
             }
+        } catch (exc: Exception) {
+            println("Please report this error in project repository issues to fix it!")
+            exc.printStackTrace()
         }
 
         return null
@@ -63,6 +68,9 @@ class FileYaml(val yamlFile: File) : SeasonYaml {
         var data: MutableMap<String, Any?> = yamlData
         for ((index, arg) in pathArray.withIndex()) {
             if (index != pathArray.lastIndex) {
+                if (!data.containsKey(arg)) {
+                    data[arg] = mutableMapOf<String, Any?>()
+                }
                 data = data[arg] as MutableMap<String, Any?>
             } else {
                 data[arg] = new
@@ -75,7 +83,7 @@ class FileYaml(val yamlFile: File) : SeasonYaml {
         var data: MutableMap<String, Any?> = yamlData
         for ((index, arg) in pathArray.withIndex()) {
             if (index != pathArray.lastIndex) {
-                data = data[arg] as MutableMap<String, Any?>
+                data = data[arg] as? MutableMap<String, Any?> ?: return
             } else {
                 data.remove(arg)
             }
